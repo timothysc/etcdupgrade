dac90688ffc9a1ac3f157f9824606011541f30c8
Hongchao Deng <hongchaodeng1@gmail.com>
2016-08-23 11:04:33 -0700

    rollback tool

--- k8s/rollback/main.go
+++ k8s/rollback/main.go
@@ -2,36 +2,61 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"path"
+	"log"
+	"strings"
 	"time"
 
-	"github.com/coreos/etcd/mvcc/backend"
-	"github.com/coreos/etcd/mvcc/mvccpb"
+	"golang.org/x/net/context"
+
+	"github.com/coreos/etcd/client"
+	"github.com/coreos/etcd/clientv3"
 )
 
-var (
-	migrateDatadir string
-)
+var etcdHosts string
 
 func init() {
-	flag.StringVar(&migrateDatadir, "data-dir", "", "Path to the data directory")
+	flag.StringVar(&etcdHosts, "etcd-addr", "http://localhost:2379", "")
 	flag.Parse()
 }
-
 func main() {
-	dbpath := path.Join(migrateDatadir, "member", "snap", "db")
+	cfg := clientv3.Config{Endpoints: strings.Split(etcdHosts, ",")}
+	etcdcli, err := clientv3.New(cfg)
+	if err != nil {
+		panic(err)
+	}
+	v2cfg := client.Config{Endpoints: strings.Split(etcdHosts, ",")}
+	v2cli, err := client.New(v2cfg)
+	if err != nil {
+		panic(err)
+	}
 
-	be := backend.New(dbpath, time.Second, 10000)
-	tx := be.BatchTx()
+	v2kv := client.NewKeysAPI(v2cli)
 
-	tx.Lock()
-	tx.UnsafeForEach([]byte("key"), func(k, v []byte) error {
-		kv := &mvccpb.KeyValue{}
-		kv.Unmarshal(v)
-		fmt.Printf("%s %d %d %d\n", string(kv.Key), kv.CreateRevision, kv.ModRevision, kv.Lease)
-		return nil
-	})
-	tx.Unlock()
+	key := "/"
+	for {
+		opts := []clientv3.OpOption{clientv3.WithLimit(100), clientv3.WithPrefix()}
+		resp, err := etcdcli.KV.Get(context.TODO(), key, opts...)
+		if err != nil {
+			panic(err)
+		}
 
+		// copy to v2
+		for _, kv := range resp.Kvs {
+			if kv.Lease != 0 {
+				v2kv.Set(context.TODO(), string(kv.Key), string(kv.Value), &client.SetOptions{
+					TTL: 1 * time.Hour,
+				})
+				continue
+			}
+			v2kv.Set(context.TODO(), string(kv.Key), string(kv.Value), nil)
+		}
+
+		if !resp.More {
+			log.Println("finished")
+			return
+		}
+
+		// move to next key
+		key = string(append(resp.Kvs[len(resp.Kvs)-1].Key, 0))
+	}
 }
